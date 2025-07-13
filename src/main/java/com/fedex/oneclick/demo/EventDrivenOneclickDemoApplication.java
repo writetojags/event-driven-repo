29683bfb8e565c84347057e22f396f489f654dd9
Siddaramaiah <j.siddaramaiah@accenture.com>
2025-07-13 10:21:13 -0500

    Event-driven-changes

---
+++ src/main/java/com/fedex/oneclick/demo/EventDrivenOneclickDemoApplication.java
@@ -0,0 +1,13 @@
+package com.fedex.oneclick.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventDrivenOneclickDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventDrivenOneclickDemoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/fedex/oneclick/demo/consumer/MessageConsumer.java
@@ -0,0 +1,14 @@
+package com.fedex.oneclick.demo.consumer;
+
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MessageConsumer {
+
+    @JmsListener(destination = "demo.queue")
+    public void receiveMessage(String message) {
+        System.out.println("Received message: " + message);
+    }
+}
+
